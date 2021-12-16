d697f237e6c240ccaf9caa46055fa3eeb599c2aa
Bruce <bruce.li@castlery.com>
2021-12-16 17:10:50 +0800

    feat(cloud-music): init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { GlobalStyle } from "./AppStyles";
+import { IconStyle } from "./assets/iconfont/iconfont";
+// import "./assets/iconfont/iconfont.css";
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <IconStyle />
+      <i className="iconfont icon-41"></i>
+    </div>
+  );
+}
+
+export default App;
