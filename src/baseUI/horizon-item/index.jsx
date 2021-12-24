f28f7bf5de3c62ed11712b9536228ae7596d9803
Bruce <bruce.li@castlery.com>
2021-12-24 11:01:13 +0800

    feat(horizon-item): add event delegation

--- src/baseUI/horizon-item/index.jsx
+++ src/baseUI/horizon-item/index.jsx
@@ -1,15 +1,40 @@
 import classNames from "classnames";
+import { useEffect, useRef } from "react";
 import Scroll from "../../components/scroll";
 import { List, ListItem } from "./style";
 
 const HorizonItem = (props) => {
   const { title = "", list = [], value = "", onClick } = props;
 
+  const ref = useRef();
+
+  useEffect(() => {
+    const node = ref.current;
+
+    const handleClick = (e) => {
+      let el = e.target;
+      while (!el.matches("span[data-key]")) {
+        if (node === el) {
+          el = null;
+          return;
+        }
+        el = el.parentNode;
+      }
+
+      onClick(el.dataset.key);
+    };
+    node.addEventListener("click", handleClick);
+    return () => {
+      node.removeEventListener("click", handleClick);
+    };
+  }, [onClick]);
+
   const listItems = list.map((c, i) => (
     <ListItem
       key={i}
       className={classNames({ selected: value === c.key })}
-      onClick={() => onClick(c.key)}
+      data-key={c.key}
+      // onClick={() => onClick(c.key)}
     >
       {c.name}
     </ListItem>
@@ -17,7 +42,7 @@ const HorizonItem = (props) => {
 
   return (
     <Scroll direction="horizontal" contentStyle={{ width: "fit-content" }}>
-      <List>
+      <List ref={ref}>
         <span>{title}</span>
         {listItems}
       </List>
